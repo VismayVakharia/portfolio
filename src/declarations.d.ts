d3001dcbb0041f8cdd61be57a86b0e64604c0ac2
Vismay Vakharia <VismayVakharia@users.noreply.github.com>
2025-04-26 03:40:46 +0530

    add about section: pic and bio

---
+++ src/declarations.d.ts
@@ -0,0 +1,8 @@
+declare module '*.png?url' {
+  const content: string;
+  export default content;
+}
+declare module '*.pdf?url' {
+  const content: string;
+  export default content;
+}
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,7 @@
 import '../styles/main.css';
+
+import { initAbout } from './about';
+
+window.addEventListener('DOMContentLoaded', async () => {
+    initAbout();
+})
